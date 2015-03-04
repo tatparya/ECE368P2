1ad191730044fc5059e9cadafef1f5cc4008df97
tatparya <tatparya.shankar@gmail.com>
2015-03-03 19:54:39 -0500

    Initialized all files

---
+++ project2.c
@@ -0,0 +1,24 @@
+/* ****************************
+**
+**  ECE 368 Project 2
+**
+**  Title: Sorting Competetion
+**
+**  Student 1: Tatparya Shankar
+**  username: tshankar
+**
+***************************** */
+
+#include "project2.h"
+
+//  Main function
+int main( )
+{
+
+}
+
+//  Main Sort Function
+void sort( int list[], int n )
+{
+
+}
